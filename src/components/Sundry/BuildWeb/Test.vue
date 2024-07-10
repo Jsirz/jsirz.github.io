32bf2078612ba312286b95c1f29bb06c0ad0dfe6
Jsir <2079571714@qq.com>
2024-07-10 23:36:24 +0800

    testPage

--- src/components/Sundry/BuildWeb/Test.vue
+++ src/components/Sundry/BuildWeb/Test.vue
@@ -7,7 +7,7 @@ import Navbar from "/src/components/HomePage/Navbar.vue"
     <Navbar />
     <BackGround />
 <div style="display: flex; justify-content: center; align-items: center; height: 80vh;">
-  <iframe src="http://localhost:5174/public/web/viewer.html?file=http://localhost:5174/public/test.pdf"
+  <iframe src="/public/web/viewer.html?file=/public/test.pdf"
           style="width: 50%; height: 100%;" frameborder="0" border="0"></iframe>
 </div>
 
